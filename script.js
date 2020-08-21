1a83f76e3b0212c51e7202e7a19a84bff021b3b3
Saurav Patel <52273867+100ravpatel@users.noreply.github.com>
2020-08-21 09:31:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+function ht() {
+    var elem = document.getElementById("ht");   
+    var width =0;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 95) {
+        clearInterval(id);
+      } else {
+        width++; 
+        elem.style.width = width + '%'; 
+        elem.innerHTML = width * 1  + '%';
+      }
+    }
+  }
+
+  /* for css3 button */ 
+
+  function cs() {
+    var elem = document.getElementById("cs");   
+    var width =0;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 85) {
+        clearInterval(id);
+      } else {
+        width++; 
+        elem.style.width = width + '%'; 
+        elem.innerHTML = width * 1  + '%';
+      }
+    }
+  }
+
+/* for javaScript button */ 
+
+  function js() {
+    var elem = document.getElementById("js");   
+    var width =0;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 70) {
+        clearInterval(id);
+      } else {
+        width++; 
+        elem.style.width = width + '%'; 
+        elem.innerHTML = width * 1  + '%';
+      }
+    }
+  }
+
+/* for Python button */ 
+
+  function py() {
+    var elem = document.getElementById("py");   
+    var width =0;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 50) {
+        clearInterval(id);
+      } else {
+        width++; 
+        elem.style.width = width + '%'; 
+        elem.innerHTML = width * 1  + '%';
+      }
+    }
+  }
+
+/* for java button */ 
+
+  function ja() {
+    var elem = document.getElementById("ja");   
+    var width =0;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 30) {
+        clearInterval(id);
+      } else {
+        width++; 
+        elem.style.width = width + '%'; 
+        elem.innerHTML = width * 1  + '%';
+      }
+    }
+  }
